055c2064be15747ee059b3d35c917afd82547c8e
shayan heidary <hdrydeveloper@gmail.com>
2023-09-26 23:08:57 +0200

    api-utils files implemented and getstaticprops is implemented in root page

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,11 +2,10 @@ import { getAllEvents } from "@/dummy-data";
 import EventList from "@/components/events/event-list";
 import EventSearch from "@/components/events/event-search";
 import { useRouter } from "next/router";
+
 const EventsPage = () => {
   const router = useRouter();
   const handlerDate = (month, year) => {
-    console.log(router);
-
     router.push(`/events/${year}/${month}/abc`);
   };
   const allEvents = getAllEvents();
